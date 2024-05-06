446f98785787277e1c25b2b332999c084e4a5e42
riru12 <brilljulianrina@gmail.com>
2024-05-07 02:49:55 +0800

    adjusted navbar colors

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -3,11 +3,11 @@
 </script>
 
 <div class="flex fixed bottom-4 md:bottom-auto md:top-4 left-2/4 translate-x-[-50%] px-4 w-screen md:w-auto">
-    <nav class="flex rounded-2xl bg-[#3c3c3c66] text-neutral-400 backdrop-blur-lg border-[1px] border-neutral-700 w-screen md:w-min p-1">
-        <a href="{base}/" class="p-4 hover:bg-[#0a0a0a66] rounded-xl grow text-center">Home</a>
-        <a href="{base}/" class="p-4 hover:bg-[#0a0a0a66] rounded-xl grow text-center">Background</a>
-        <a href="{base}/" class="p-4 hover:bg-[#0a0a0a66] rounded-xl grow text-center">Data</a>
-        <a href="{base}/" class="p-4 hover:bg-[#0a0a0a66] rounded-xl grow text-center">Methodology</a>
-        <a href="{base}/" class="p-4 hover:bg-[#0a0a0a66] rounded-xl grow text-center">Results</a>
+    <nav class="flex rounded-2xl bg-[#2d2d2d99] text-neutral-300 font-semibold backdrop-blur-lg border-[1px] border-neutral-700 w-screen md:w-min p-1">
+        <a href="{base}/" class="p-4 hover:bg-[#a3a3a336] rounded-xl grow text-center">Home</a>
+        <a href="{base}/" class="p-4 hover:bg-[#a3a3a336] rounded-xl grow text-center">Background</a>
+        <a href="{base}/" class="p-4 hover:bg-[#a3a3a336] rounded-xl grow text-center">Data</a>
+        <a href="{base}/" class="p-4 hover:bg-[#a3a3a336] rounded-xl grow text-center">Methodology</a>
+        <a href="{base}/" class="p-4 hover:bg-[#a3a3a336] rounded-xl grow text-center">Results</a>
     </nav>
 </div>
