73c809965b1b68e2f6a669c0022888c4b604d383
riru12 <brilljulianrina@gmail.com>
2024-05-07 02:35:05 +0800

    adjusted navbar padding

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="flex fixed bottom-4 md:bottom-auto md:top-4 left-2/4 translate-x-[-50%] px-4 w-screen md:w-auto">
-    <nav class="flex rounded-2xl bg-[#3c3c3c66] text-neutral-400 backdrop-blur-lg border-[1px] border-neutral-700 w-screen md:w-min p-2">
+    <nav class="flex rounded-2xl bg-[#3c3c3c66] text-neutral-400 backdrop-blur-lg border-[1px] border-neutral-700 w-screen md:w-min p-1">
         <a href="{base}/" class="p-4 hover:bg-[#0a0a0a66] rounded-xl grow text-center">Home</a>
         <a href="{base}/" class="p-4 hover:bg-[#0a0a0a66] rounded-xl grow text-center">Background</a>
         <a href="{base}/" class="p-4 hover:bg-[#0a0a0a66] rounded-xl grow text-center">Data</a>
