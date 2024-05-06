f560cfe02cc7c127bdee9b07d59784cf1b01a47d
riru12 <brilljulianrina@gmail.com>
2024-05-07 04:06:25 +0800

    🚚 moved assets to /lib directory

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,6 +1,6 @@
 <script>
-    import heroimg from "../assets/images/hero.jpg";
-    import "../assets/css/gray.css"
+    import heroimg from "../lib/images/hero.jpg";
+    import "../lib/css/gray.css"
 </script>
 
 <section id="hero">
