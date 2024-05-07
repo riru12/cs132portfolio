f273a6dc08b4132c5f69843c0d33e19c4b134783
riru12 <brilljulianrina@gmail.com>
2024-05-07 12:31:56 +0800

    changed to snap-mandatory

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,12 @@
   import Navbar from "../components/Navbar.svelte";
   import Hero from "../components/Hero.svelte";
   import Overview from "../components/Overview.svelte";
-    import Overviewcopy from "../components/Overview copy.svelte";
 </script>
 
 <slot />
-<div class="snap-y snap-normal overflow-y-scroll h-screen flex-grow z-0">
+<div class="snap-y snap-mandatory overflow-y-scroll h-screen flex-grow z-0">
   <Navbar/>
   <Hero />
   <Overview />
-  <Overviewcopy />
+  <Overview />
 </div>
