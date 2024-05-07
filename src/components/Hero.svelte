36df45fa89b4231c26d2ca51a60e078e09068a5f
riru12 <brilljulianrina@gmail.com>
2024-05-07 11:52:24 +0800

    💄 added top black gradient

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -5,8 +5,9 @@
 
 <section id="hero">
         <div class="relative w-screen h-screen bg-black overflow-hidden">
+            <div class="absolute h-32 w-full top-0 bg-gradient-to-b from-black to-transparent z-40 pointer-events-none"></div>
             <div class="absolute z-40 left-1/2 translate-x-[-50%] top-1/2 translate-y-[-50%]">
-                <div class="text-white font-bold text-6xl md:text-8xl">
+                <div class="text-white font-bold text-6xl lg:text-7xl xl:text-8xl">
                     <span class="sdgred">One </span>in five people in the Philippines lives in <span class="sdgred">extreme poverty</span>.
                 </div>
             </div>
