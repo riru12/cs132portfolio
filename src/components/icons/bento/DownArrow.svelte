f85a4f7e4f2581b9a2decc652c0a85b8548042ad
riru12 <brilljulianrina@gmail.com>
2024-05-08 04:33:28 +0800

    📱 responsive bento

--- src/components/icons/bento/DownArrow.svelte
+++ src/components/icons/bento/DownArrow.svelte
@@ -1,4 +1,4 @@
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-8 h-8">
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-8 h-8 hover:stroke-[#f8ff00]">
     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
 </svg>
   
