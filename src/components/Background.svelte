9e9b6af673e1e8b0389de8277dc7c99544d3a9e4
Jeff <jgpecson@up.edu.ph>
2024-05-07 14:17:38 +0800

    make initial background

---
+++ src/components/Background.svelte
@@ -0,0 +1,16 @@
+<section id="Overview" class="mx-12 md:mx-64 py-24 min-h-svh justify-center flex flex-col snap-always snap-start">
+    <label for="Title" class="text-gray-500 italic">
+        Background
+    </label>
+    <div id="Title" class="text-white font-bold text-4xl">
+        Over <span class="sdgred">25 million Filipinos</span> are under <span class="sdgred">poverty</span>
+    </div>
+    <div class="text-white">
+        Despite the government's interventions in addressing poverty, the Philippine Statistics Authority (PSA) Undersecretary 
+        and National Statistician Dennis Mapa stated that over 25.24 million Filipinos remain under the poverty line. Within this 
+        population, over 9.79 million of Filipinos are unable to meet their basic food needs. Moreover, the National Economic and 
+        Development Authority (NEDA) estimated that around 7.2 million Filipinos are unable to escape poverty as they are trapped 
+        by the inflation rate in the country. Moreover, the recent COVID-19 pandemic further exacerbated the issue, pushing many 
+        more into poverty.
+    </div>
+</section>
