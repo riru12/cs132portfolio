941938492516ef521a3687b517277402f68233c6
riru12 <brilljulianrina@gmail.com>
2024-05-08 18:47:24 +0800

    🔥 removed old overview

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,6 @@
   import "../app.css";
   import Navbar from "../components/Navbar.svelte";
   import Bento from "../components/Bento.svelte";
-  import Overview from "../components/Overview.svelte";
   import Background from "../components/Background.svelte";
   import Background2 from "../components/Background-2.svelte";
   import Data from "../components/Data.svelte";
@@ -12,7 +11,6 @@
 <div class="sm:snap-y sm:snap-mandatory sm:overflow-y-scroll h-screen flex-grow z-0">
   <Navbar/>
   <Bento />
-  <!-- <Overview /> -->
   <Background />
   <Background2 />
   <Data />
