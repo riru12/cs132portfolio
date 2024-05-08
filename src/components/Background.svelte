7a308b1ef26425a0994f165e536710d2874125b8
riru12 <brilljulianrina@gmail.com>
2024-05-08 16:56:38 +0800

    revised background section

--- src/components/Background.svelte
+++ src/components/Background.svelte
@@ -1,21 +1,59 @@
-<section id="background" class="mx-12 md:mx-32 lg:mx-64 py-16 min-h-auto md:min-h-screen justify-center flex flex-col sm:snap-always sm:snap-start">
-    <div class="flex flex-row gap-2 mb-10">
-        <div class="green-text text-3xl font-bold">
+<script>
+    import article1 from "$lib/images/article1.jpg";
+    import article2 from "$lib/images/article2.jpg";
+    import article3 from "$lib/images/article3.jpg";
+    import article4 from "$lib/images/article4.jpg";
+    import bento3 from "$lib/images/bento3.jpg";
+</script>
+
+<section id="background" class="py-16 min-h-auto md:min-h-screen justify-center flex flex-col sm:snap-always sm:snap-start">
+    <div class="flex flex-row gap-2 mb-10 justify-center">
+        <div class="green-text text-3xl font-bold gradient-text">
             02
         </div>
         <div class="text-white text-3xl font-bold">
             Background
         </div>
     </div>
-    <div class="text-white font-bold text-2xl lg:text-3xl xl:text-4xl mb-5">
-        Over <span class="green-text">25 million Filipinos</span> are under <span class="green-text">poverty</span>
-    </div>
-    <div class="gray-text text-xl xl:text-2xl">
-        Despite the government's interventions in addressing poverty, the Philippine Statistics Authority (PSA) Undersecretary 
-        and National Statistician Dennis Mapa stated that over 25.24 million Filipinos remain under the poverty line. Within this 
-        population, over 9.79 million of Filipinos are unable to meet their basic food needs. Moreover, the National Economic and 
-        Development Authority (NEDA) estimated that around 7.2 million Filipinos are unable to escape poverty as they are trapped 
-        by the inflation rate in the country. Moreover, the recent COVID-19 pandemic further exacerbated the issue, pushing many 
-        more into poverty.
+    <div class="flex gap-4 overflow-x-scroll px-20">
+        <div class="flex relative rounded-3xl min-h-[40rem] max-w-[24rem] bg-[#232323] shrink-0">
+            <a href="https://www.pna.gov.ph/articles/1215836" target="_blank">
+                <div class="absolute z-10 left-8 top-8 right-8 text-white">
+                    <p>Philippine News Agency</p>
+                    <p class="text-3xl font-bold">PH poverty rate declines to 22.4% in 1st half 2023</p>
+                </div>
+            </a>
+            <img src={article1} alt="" class="min-h-full md:min-w-full object-cover rounded-[1.5rem] darker-gray">
+        </div>
+        <div class="flex relative rounded-3xl min-h-[40rem] max-w-[24rem] bg-[#232323] shrink-0">
+            <a href="https://mb.com.ph/2023/12/22/high-inflation-traps-over-7-m-filipinos-in-poverty-1" target="_blank">
+                <div class="absolute z-10 left-8 top-8 right-8 text-white">
+                    <p>Manila Bulletin</p>
+                    <p class="text-3xl font-bold">High inflation traps over 7 M Filipinos in poverty</p>
+                </div>
+            </a>
+            <img src={article2} alt="" class="min-h-full md:min-w-full object-cover rounded-[1.5rem] darker-gray">
+        </div>
+        <div class="flex relative rounded-3xl min-h-[40rem] max-w-[24rem] bg-[#232323] shrink-0">
+            <a href="https://www.reuters.com/world/asia-pacific/pandemic-pushed-millions-more-into-poverty-philippines-govt-2022-08-15/" target="_blank">
+                <div class="absolute z-10 left-8 top-8 right-8 text-white">
+                    <p>Reuters</p>
+                    <p class="text-3xl font-bold">Pandemic pushed millions more into poverty in the Philippines, government says</p>
+                </div>
+            </a>
+            <img src={article3} alt="" class="min-h-full md:min-w-full object-cover rounded-[1.5rem] darker-gray">
+        </div>
+        <div class="flex relative rounded-3xl min-h-[40rem] max-w-[24rem] bg-[#232323] shrink-0">
+            <a href="https://phkule.org/article/1097/poorest-filipinos-affected-more-by-high-inflation-ibon" target="_blank">
+                <div class="absolute z-10 left-8 top-8 right-8 text-white">
+                    <p>Philippine Collegian</p>
+                    <p class="text-3xl font-bold">Poorest Filipinos Affected More by High Inflation—IBON</p>
+                </div>
+            </a>
+            <img src={article4} alt="" class="min-h-full md:min-w-full object-cover rounded-[1.5rem] darker-gray">
+        </div>
+        <div class="flex justify-center items-center rounded-3xl min-h-[40rem] max-w-[24rem] bg-[#141414] shrink-0">
+            <div class="text-white font-bold text-2xl text-center p-16">...steps are being taken, but further action may be <span class="gradient-text">needed</span>.</div>
+        </div>
     </div>
 </section>
