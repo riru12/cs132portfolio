b02aaa7583b263fea2028ce10489fbc6fd2ff8e4
riru12 <brilljulianrina@gmail.com>
2024-05-08 17:12:30 +0800

    adjusted background card heights

--- src/components/Background.svelte
+++ src/components/Background.svelte
@@ -15,45 +15,49 @@
             Background
         </div>
     </div>
-    <div class="flex gap-4 overflow-x-scroll px-20">
-        <div class="flex relative rounded-3xl min-h-[40rem] max-w-[24rem] bg-[#232323] shrink-0">
-            <a href="https://www.pna.gov.ph/articles/1215836" target="_blank">
-                <div class="absolute z-10 left-8 top-8 right-8 text-white">
-                    <p>Philippine News Agency</p>
-                    <p class="text-3xl font-bold">PH poverty rate declines to 22.4% in 1st half 2023</p>
-                </div>
-            </a>
-            <img src={article1} alt="" class="min-h-full md:min-w-full object-cover rounded-[1.5rem] darker-gray">
+    <div class="relative w-full h-full">
+        <div class="absolute bg-gradient-to-l from-black to-transparent h-full w-24 right-0 z-50"></div>
+        <div class="flex relative gap-4 overflow-x-scroll px-20">
+            <div class="flex relative rounded-3xl min-h-[32rem] max-w-[24rem] bg-[#232323] shrink-0">
+                <a href="https://www.pna.gov.ph/articles/1215836" target="_blank">
+                    <div class="absolute z-10 left-8 top-8 right-8 text-white">
+                        <p>Philippine News Agency</p>
+                        <p class="text-3xl font-bold">PH poverty rate declines to 22.4% in 1st half 2023</p>
+                    </div>
+                </a>
+                <img src={article1} alt="" class="min-h-full md:min-w-full object-cover rounded-[1.5rem] darker-gray">
+            </div>
+            <div class="flex relative rounded-3xl min-h-[32rem] max-w-[24rem] bg-[#232323] shrink-0">
+                <a href="https://mb.com.ph/2023/12/22/high-inflation-traps-over-7-m-filipinos-in-poverty-1" target="_blank">
+                    <div class="absolute z-10 left-8 top-8 right-8 text-white">
+                        <p>Manila Bulletin</p>
+                        <p class="text-3xl font-bold">High inflation traps over 7 M Filipinos in poverty</p>
+                    </div>
+                </a>
+                <img src={article2} alt="" class="min-h-full md:min-w-full object-cover rounded-[1.5rem] darker-gray">
+            </div>
+            <div class="flex relative rounded-3xl min-h-[32rem] max-w-[24rem] bg-[#232323] shrink-0">
+                <a href="https://www.reuters.com/world/asia-pacific/pandemic-pushed-millions-more-into-poverty-philippines-govt-2022-08-15/" target="_blank">
+                    <div class="absolute z-10 left-8 top-8 right-8 text-white">
+                        <p>Reuters</p>
+                        <p class="text-3xl font-bold">Pandemic pushed millions more into poverty in the Philippines, government says</p>
+                    </div>
+                </a>
+                <img src={article3} alt="" class="min-h-full md:min-w-full object-cover rounded-[1.5rem] darker-gray">
+            </div>
+            <div class="flex relative rounded-3xl min-h-[32rem] max-w-[24rem] bg-[#232323] shrink-0">
+                <a href="https://phkule.org/article/1097/poorest-filipinos-affected-more-by-high-inflation-ibon" target="_blank">
+                    <div class="absolute z-10 left-8 top-8 right-8 text-white">
+                        <p>Philippine Collegian</p>
+                        <p class="text-3xl font-bold">Poorest Filipinos Affected More by High Inflation—IBON</p>
+                    </div>
+                </a>
+                <img src={article4} alt="" class="min-h-full md:min-w-full object-cover rounded-[1.5rem] darker-gray">
+            </div>
+            <div class="flex justify-center items-center rounded-3xl min-h-[32rem] max-w-[24rem] bg-[#141414] shrink-0">
+                <div class="text-white font-bold text-2xl gray-text text-center p-16">...steps are being taken, but <span class="text-white">further action</span> may be <span class="gradient-text">needed</span>.</div>
+            </div>
         </div>
-        <div class="flex relative rounded-3xl min-h-[40rem] max-w-[24rem] bg-[#232323] shrink-0">
-            <a href="https://mb.com.ph/2023/12/22/high-inflation-traps-over-7-m-filipinos-in-poverty-1" target="_blank">
-                <div class="absolute z-10 left-8 top-8 right-8 text-white">
-                    <p>Manila Bulletin</p>
-                    <p class="text-3xl font-bold">High inflation traps over 7 M Filipinos in poverty</p>
-                </div>
-            </a>
-            <img src={article2} alt="" class="min-h-full md:min-w-full object-cover rounded-[1.5rem] darker-gray">
-        </div>
-        <div class="flex relative rounded-3xl min-h-[40rem] max-w-[24rem] bg-[#232323] shrink-0">
-            <a href="https://www.reuters.com/world/asia-pacific/pandemic-pushed-millions-more-into-poverty-philippines-govt-2022-08-15/" target="_blank">
-                <div class="absolute z-10 left-8 top-8 right-8 text-white">
-                    <p>Reuters</p>
-                    <p class="text-3xl font-bold">Pandemic pushed millions more into poverty in the Philippines, government says</p>
-                </div>
-            </a>
-            <img src={article3} alt="" class="min-h-full md:min-w-full object-cover rounded-[1.5rem] darker-gray">
-        </div>
-        <div class="flex relative rounded-3xl min-h-[40rem] max-w-[24rem] bg-[#232323] shrink-0">
-            <a href="https://phkule.org/article/1097/poorest-filipinos-affected-more-by-high-inflation-ibon" target="_blank">
-                <div class="absolute z-10 left-8 top-8 right-8 text-white">
-                    <p>Philippine Collegian</p>
-                    <p class="text-3xl font-bold">Poorest Filipinos Affected More by High Inflation—IBON</p>
-                </div>
-            </a>
-            <img src={article4} alt="" class="min-h-full md:min-w-full object-cover rounded-[1.5rem] darker-gray">
-        </div>
-        <div class="flex justify-center items-center rounded-3xl min-h-[40rem] max-w-[24rem] bg-[#141414] shrink-0">
-            <div class="text-white font-bold text-2xl text-center p-16">...steps are being taken, but further action may be <span class="gradient-text">needed</span>.</div>
-        </div>
+
     </div>
 </section>
