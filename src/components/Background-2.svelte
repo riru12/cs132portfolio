7c7b39752ec39220b35e91383bdd2995330c9a4f
riru12 <brilljulianrina@gmail.com>
2024-05-10 16:24:43 +0800

    added hover effect to boxes

--- src/components/Background-2.svelte
+++ src/components/Background-2.svelte
@@ -7,19 +7,19 @@
             What are the factors that <span class="gradient-text">most negatively</span> affect Filipino households' finances and increase risk of poverty?
         </div>
         <div class="grid grid-cols-1 2xl:grid-cols-3 gap-4 2xl:gap-8">
-            <div class="grid grid-cols-1 bg-[#232323] rounded-3xl p-4 2xl:p-6 text-white max-w-96 2xl:max-w-auto text-lg lg:text-xl">
+            <div class="grid grid-cols-1 bg-[#232323] rounded-3xl p-4 2xl:p-6 text-white max-w-96 2xl:max-w-auto text-lg lg:text-xl hover:bg-[#323232] hover:border-[1px] hover:border-[#575757]">
                 <p class="gradient-text p-2 font-bold text-center content-end text-xl lg:text-2xl">
                     Null Hypothesis
                 </p>
                 <p class="p-2 text-center text-white text-base 2xl:text-lg">There is no significant correlation between household factors and financial well-being among families</p>
             </div>
-            <div class="grid grid-cols-1 bg-[#232323] rounded-3xl p-4 2xl:p-6 text-white max-w-96 2xl:max-w-auto text-lg lg:text-xl">
+            <div class="grid grid-cols-1 bg-[#232323] rounded-3xl p-4 2xl:p-6 text-white max-w-96 2xl:max-w-auto text-lg lg:text-xl hover:bg-[#323232] hover:border-[1px] hover:border-[#575757]">
                 <p class="gradient-text p-2 font-bold text-center content-end text-xl lg:text-2xl">
                     Alternative Hypothesis
                 </p>
                 <p class="p-2 text-center text-white text-base 2xl:text-lg">There is a significant negative correlation between household factors and financial well-being among families</p>
             </div>
-            <div class="grid grid-cols-1 bg-[#232323] rounded-3xl p-4 2xl:p-6 text-white max-w-96 2xl:max-w-auto text-lg lg:text-xl">
+            <div class="grid grid-cols-1 bg-[#232323] rounded-3xl p-4 2xl:p-6 text-white max-w-96 2xl:max-w-auto text-lg lg:text-xl hover:bg-[#323232] hover:border-[1px] hover:border-[#575757]">
                 <p class="gradient-text p-2 font-bold text-center content-end text-xl lg:text-2xl">
                     Action Plan
                 </p>
