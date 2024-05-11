fa85d50e73ec0e97eb940b9b18c66dadc48e1d07
riru12 <brilljulianrina@gmail.com>
2024-05-12 03:02:24 +0800

    method drawers implemented

--- src/components/Method.svelte
+++ src/components/Method.svelte
@@ -1,4 +1,19 @@
-<section id="method" class="mx-12 md:mx-32 lg:mx-64 py-32 min-h-screen justify-center flex flex-col">
+<script>
+    import "$lib/css/method.css";
+    import PlusIcon from "./icons/methods/PlusIcon.svelte";
+
+    function handleSections(event){
+        if (event.type === 'click' || (event.type === 'keydown' && (event.key === 'Enter' || event.key === ' '))) {
+            const section = event.target.closest('.subsection');
+            if (section.getAttribute("data-section")){
+                section.classList.toggle("active");
+            }
+        }
+    }
+</script>
+
+
+<section id="method" class="mx-12 md:mx-32 lg:mx-64 py-32 flex flex-col">
     <div class="flex flex-row gap-2 mb-10 justify-center">
         <div class="green-text text-3xl font-bold gradient-text">
             04
@@ -7,7 +22,51 @@
             Methodology
         </div>
     </div>
-    <div class="gray-text text-xl xl:text-2xl text-center">
-        Hold on, we're still cooking this one up! Come back in the future to see updates
+    <div on:click={handleSections} on:keydown={handleSections} role="none" class="flex flex-col items-content gap-2">
+        <div data-section=1 class="subsection" tabindex="0" role="button">
+            <div class="flex flex-row justify-between items-center px-4">
+                <div class="font-bold p-2">
+                    Data Preprocessing
+                </div>
+                <div class="h-min w-min p-2 rounded-[50%] hover:bg-black">
+                    <PlusIcon/>
+                </div>
+            </div>
+            <div class="">
+                <div class="p-6">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </div>
+            </div>
+        </div>
+        <div data-section=2 class="subsection" tabindex="0" role="button">
+            <div class="flex flex-row justify-between items-center px-4">
+                <div class="font-bold p-2">
+                    Visualization
+                </div>
+                <div class="h-min w-min p-2 rounded-[50%] hover:bg-black">
+                    <PlusIcon/>
+                </div>
+            </div>
+            <div class="">
+                <div class="p-6">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </div>
+            </div>
+        </div>
+        <div data-section=3 class="subsection" tabindex="0" role="button">
+            <div class="flex flex-row justify-between items-center px-4">
+                <div class="font-bold p-2">
+                    Graphs
+                </div>
+                <div class="h-min w-min p-2 rounded-[50%] hover:bg-black">
+                    <PlusIcon/>
+                </div>
+            </div>
+            <div class="">
+                <div class="p-6">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </div>
+            </div>
+        </div>
     </div>
 </section>
