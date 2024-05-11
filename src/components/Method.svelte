16765edc9373487085fbd4e26524cd2647bba763
riru12 <brilljulianrina@gmail.com>
2024-05-12 03:25:05 +0800

    animation adjustment, added preprocessing info

--- src/components/Method.svelte
+++ src/components/Method.svelte
@@ -34,7 +34,18 @@
             </div>
             <div class="">
                 <div class="p-6">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    <div class="py-2">
+                        <h2 class="text-lg py-2 font-semibold">Handling Missing/Null Values</h2>
+                        <p>The dataset from PSA was checked for null/missing values. As mentioned, the dataset contains a total of 165,029 entries and 88 features and upon identifying any null values, it was confirmed that there there were neither missing nor null values within the dataset.</p>
+                    </div>
+                    <div class="py-2">
+                        <h2 class="text-lg py-2 font-semibold">Sampling the Data</h2>
+                        <p>Due to the huge volume of data, processing it would be time and resource heavy. So, we sampled 10% of the original volume of the dataset, leaving us with a sampled dataset containing 16,503 entries. Check out the sampled dataset in the Data section above!</p>
+                    </div>
+                    <div class="py-2">
+                        <h2 class="text-lg py-2 font-semibold">Addition and Dropping of Certain Features</h2>
+                        <p>We added a column which computes for the net income of a family, equal to their total income minus their expenditures. We also dropped certain features (columns) that are redundant and not related to the study such as some of the elements in the breakdowns of the total income and a few nominal features.</p>
+                    </div>
                 </div>
             </div>
         </div>
