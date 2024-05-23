608016e9b820b736d10faa6cbbd61d604b26986b
riru12 <brilljulianrina@gmail.com>
2024-05-24 03:00:09 +0800

    added empty card for team member, adjusted methods content

--- src/components/Method.svelte
+++ src/components/Method.svelte
@@ -34,28 +34,25 @@
                 <div>
                     <h2>Handling Missing/Null Values</h2>
                     <p>The dataset was checked for null/missing values. The dataset contains a total of 165,029 entries and 88 features and it was confirmed that the dataset does not contain missing or null values as its dimensions remained the same after processing.</p>
-                    <h2>Sampling the Data</h2>
-                    <p>Due to the huge volume of data, processing it would be time and resource heavy. So, we sampled 10% of the original volume which can be seen in the data section, leaving us with a sampled dataset containing 16,503 entries.</p>
+                    <h2>Addition of Features</h2>
+                    <p>We added certain features which we would use in our statistical analysis, such as a binary column to indicate whether a household receives wages and another if they own or operate a business. We also added a column that computes the net total of a family every month, and each family's financial category.</p>
                     <h2>Dropping Certain Features</h2>
-                    <p>We dropped certain features (columns) that are redundant and not related to the study such as some of the elements in the breakdowns of the total income and a few nominal features.</p>
-                    <!-- <DummyChart /> -->
-                    <BarError />
+                    <p>However, we also dropped certain features that are redundant and not related to the study such as some of the elements in the breakdowns of the total income and a few nominal features.</p>
+                    <h2>Sampling the Data</h2>
+                    <p>Due to the huge volume of data, processing it would be time and resource heavy. So, we sampled 10% of the original volume which can be seen in the data section, leaving us with a sampled dataset containing 16,503 entries. We ensured that our sampled data is balanced as per the financial category feature, which means that in our sample there are 5501 entries of households per financial category.</p>
                 </div>
             </div>
         </div>
         <div data-section=2 class="subsection">
             <div class="flex w-full justify-between items-center p-3 font-semibold clickable">
-                Data Preprocessing
+                Exploratory Data Analysis
                 <div class="w-min h-min rounded-full p-1 hover:bg-black"><PlusIcon /></div>
             </div>
             <div> <!-- 1-ROW GRID -->
                 <div>
-                    <h2>Handling Missing/Null Values</h2>
-                    <p>The dataset was checked for null/missing values. The dataset contains a total of 165,029 entries and 88 features and it was confirmed that the dataset does not contain missing or null values as its dimensions remained the same after processing.</p>
-                    <h2>Sampling the Data</h2>
-                    <p>Due to the huge volume of data, processing it would be time and resource heavy. So, we sampled 10% of the original volume which can be seen in the data section, leaving us with a sampled dataset containing 16,503 entries.</p>
-                    <h2>Dropping Certain Features</h2>
-                    <p>We dropped certain features (columns) that are redundant and not related to the study such as some of the elements in the breakdowns of the total income and a few nominal features.</p>
+                    <h2>Feature Selection</h2>
+                    <p>Utilizing Random Forest Classifier, we identified the features that impacts a Filipino household's finances the most. The graph below shows the importance value of the top ten identified features.</p>
+                    <BarError />
                 </div>
             </div>
         </div>
--- src/components/charts/BarError.svelte
+++ src/components/charts/BarError.svelte
@@ -2,7 +2,7 @@
 	import { BarWithErrorBarsChart } from 'chartjs-chart-error-bars';
 	import { onMount } from 'svelte';
     
-	let Labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday' ];
+	let Labels = ['NFOOD', 'HOUSING_WATER', 'BREAD', 'MEAT', 'HEALTH', 'TRANSPORT', 'MISCELLANEOUS', 'VEG', 'SUGAR', 'FURNISHING'];
 	let chartCanvas;
 
     let config = {
@@ -12,39 +12,54 @@
             label: 'RFE Feature Importance Score',
             data: [
                 {
-                    y: 4,
-                    yMin: 1,
-                    yMax: 6,
+                    y: 0.108420,
+                    yMin: 0.10009505,
+                    yMax: 0.11674495,
                 },
                 {
-                    y: 2,
-                    yMin: 1,
-                    yMax: 4,
+                    y: 0.105144,
+                    yMin: 0.09790631,
+                    yMax: 0.11238169,
                 },
                 {
-                    y: 4,
-                    yMin: 1,
-                    yMax: 6,
+                    y: 0.101487,
+                    yMin: 0.09597465,
+                    yMax: 0.10699935,
                 },
                 {
-                    y: 2,
-                    yMin: 1,
-                    yMax: 4,
+                    y: 0.099369,
+                    yMin: 0.09291254,
+                    yMax: 0.10582546,
                 },
                 {
-                    y: 4,
-                    yMin: 1,
-                    yMax: 6,
+                    y: 0.098867,
+                    yMin: 0.09310979,
+                    yMax: 0.10462421,
                 },
                 {
-                    y: 2,
-                    yMin: 1,
-                    yMax: 4,
+                    y: 0.098350,
+                    yMin: 0.09266503,
+                    yMax: 0.10403497,
                 },
                 {
-                    y: 4,
-                    yMin: 1,
-                    yMax: 6,
+                    y: 0.098074,
+                    yMin: 0.09270537,
+                    yMax: 0.10344263,
+                },
+                {
+                    y: 0.097430,
+                    yMin: 0.0918136,
+                    yMax: 0.1030464,
+                },
+                {
+                    y: 0.096811,
+                    yMin: 0.09167778,
+                    yMax: 0.10194422,
+                },
+                {
+                    y: 0.096048,
+                    yMin: 0.09086223,
+                    yMax: 0.10123377,
                 },
             ],
             backgroundColor: '#47C69A',
@@ -77,4 +92,4 @@
 		});
 	});
 </script>
-<canvas bind:this={chartCanvas} id="myChart" class="py-4 px-4 mx-24 rounded-lg"></canvas>
+<canvas bind:this={chartCanvas} id="myChart" class="py-4 px-4 2xl:mx-24"></canvas>
