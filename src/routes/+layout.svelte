0dbd8c8fc6ba1c6b578abda38c93f107b5c9bfaa
riru12 <brilljulianrina@gmail.com>
2024-05-23 23:37:34 +0800

    moved some code around

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,8 @@
-<script>
-  import "../app.css";
-  import Navbar from "../components/Navbar.svelte";
-  import Bento from "../components/Bento.svelte";
-  import Background from "../components/Background.svelte";
-  import Background2 from "../components/Background-2.svelte";
-  import Data from "../components/Data.svelte";
-  import Method from "../components/Method.svelte";
-  import Team from "../components/Team.svelte";
-  // import Test from "../components/Test.svelte"
-</script>
+<style lang="postcss">
+  :global(html) {
+    background-color: black;
+  }
+</style>
+
 
 <slot />
-<div>
-  <!-- <Test /> -->
-  <Navbar/>
-  <Bento />
-  <Background />
-  <Background2 />
-  <Data />
-  <Method />
-  <Team />
-</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,22 @@
-<style lang="postcss">
-  :global(html) {
-    background-color: black;
-  }
-</style>
+<script>
+  import "../app.css";
+  import Navbar from "../components/Navbar.svelte";
+  import Bento from "../components/Bento.svelte";
+  import Background from "../components/Background.svelte";
+  import Background2 from "../components/Background-2.svelte";
+  import Data from "../components/Data.svelte";
+  import Method from "../components/Method.svelte";
+  import Team from "../components/Team.svelte";
+  // import Test from "../components/Test.svelte"
+</script>
+
+<div>
+  <!-- <Test /> -->
+  <Navbar/>
+  <Bento />
+  <Background />
+  <Background2 />
+  <Data />
+  <Method />
+  <Team />
+</div>
